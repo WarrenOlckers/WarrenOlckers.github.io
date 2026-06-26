911bf2e0064283d79d27c6861498a0006ec15ff0
WarrenOlckers <warrenolckers@gmail.com>
2026-06-26 13:38:43 +0200

    email direction test2

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const contactButton = document.getElementById("contactButton");
+contactButton.addEventListener("click", function(){
+    window.location.href = "mailto:warrenolckers@gmail.com";
+});
