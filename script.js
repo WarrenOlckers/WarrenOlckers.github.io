50fab27fdce857e7c1b0f4be6f1168ca4c2cb7a4
WarrenOlckers <warrenolckers@gmail.com>
2026-06-28 15:04:09 +0200

    last styling before adding functionality

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active');
+}
+
 
 const contactButton = document.getElementById("contactButton");
 contactButton.addEventListener("click", function(){
